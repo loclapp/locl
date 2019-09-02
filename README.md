cc8a3981a21c711e3197d71a0285c9dc9e8bb183
Olivier Combe <olivier.combe@gmail.com>
2019-09-02 16:44:03 +0200

    chore: cleanup packages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center"><img src="https://raw.githubusercontent.com/loclapp/locl/master/apps/web/src/assets/img/locl-social.png" width="450"></p>
 
-🌍 **Locl is an internationalization (i18n) tools suite for Angular.**
+🌐 **Locl is an internationalization (i18n) tools suite for Angular.**
 
 More documentation soon...
--- libs/common/src/lib/common.module.spec.ts
+++ libs/common/src/lib/common.module.spec.ts
@@ -1,14 +1,14 @@
 import { async, TestBed } from '@angular/core/testing';
-import { CommonModule } from './common.module';
+import { LoclCommonModule } from './common.module';
 
 describe('CommonModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [CommonModule]
+      imports: [LoclCommonModule]
     }).compileComponents();
   }));
 
   it('should create', () => {
-    expect(CommonModule).toBeDefined();
+    expect(LoclCommonModule).toBeDefined();
   });
 });
