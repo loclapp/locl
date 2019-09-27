ea2738a3f6b3777be2cb7ae83e8011e3496ba896
Olivier Combe <olivier.combe@gmail.com>
2019-09-27 15:15:22 +0200

    chore: updating dependencies

--- testing/test.libs.ts
+++ testing/test.libs.ts
@@ -3,7 +3,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import 'core-js/es/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 
